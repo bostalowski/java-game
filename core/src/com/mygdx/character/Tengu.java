4a2167a161a3c9b1c1203ada0e0d36fc1bcb0c00
bostalowski <bostalowski@kaliop.com>
2015-08-25 19:54:28 +0200

    change controllers handler

--- core/src/com/mygdx/character/Tengu.java
+++ core/src/com/mygdx/character/Tengu.java
@@ -1,22 +1,15 @@
 package com.mygdx.character;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.ControllerListener;
-import com.badlogic.gdx.controllers.Controllers;
-import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.mygdx.character.animations.CharacterAnimation;
 import com.mygdx.character.animations.CharacterJumpAnimation;
 import com.mygdx.character.animations.CharacterRunAnimation;
 import com.mygdx.character.animations.CharacterStanceAnimation;
 import com.mygdx.tools.Physics;
-import com.mygdx.tools.XBox360Pad;
 
-public class Tengu extends Physics implements ControllerListener
+public class Tengu extends Physics
 {
     private static final int ACTION_STANCE = 0;
     private static final int ACTION_RUN = 1;
@@ -45,12 +38,11 @@ public class Tengu extends Physics implements ControllerListener
             isJumping,
             hasDoubleJumped,
             isDashingLeft,
-            isDashingRight;
+            isDashingRight,
+            isAnyKeyPressed;
 
     public static final float DASH_MAX_DISTANCE = 300f;
     private float elapsedDashDistance;
-
-    Controller controller;
 
     public Tengu(Vector2 position, float speed, float direction)
     {
@@ -66,6 +58,7 @@ public class Tengu extends Physics implements ControllerListener
         this.isDashingLeft = false;
         this.isDashingRight = false;
         this.elapsedDashDistance = 0;
+        this.isAnyKeyPressed = false;
 
         this.characterStanceAnimation = new CharacterStanceAnimation(this);
         this.characterRunAnimation = new CharacterRunAnimation(this);
@@ -73,15 +66,17 @@ public class Tengu extends Physics implements ControllerListener
 
         this.currentAction = ACTION_STANCE;
         this.currentAnimation = this.characterStanceAnimation;
-
-        // Listen to all controllers, not just one
-        Controllers.addListener(this);
-
-        this.controller = Controllers.getControllers().first();
     }
 
     public void update(float deltaTime)
     {
+        if(!this.isAnyKeyPressed && !this.isJumping) {
+            if(Math.round(this.velocity.x * 100) == 0) {
+                this.velocity.x = 0;
+                changeAnimation(ACTION_STANCE);
+            }
+        }
+
         if(isOnGround) {
             this.friction = new Vector2(FRICTION_ON_GROUND, 0);
             this.isJumping = false;
@@ -121,6 +116,7 @@ public class Tengu extends Physics implements ControllerListener
         }
 
         this.currentAnimation.update();
+        this.isAnyKeyPressed = false;
     }
 
     public void render()
@@ -128,87 +124,79 @@ public class Tengu extends Physics implements ControllerListener
         this.currentAnimation.render();
     }
 
-    public void handleKeys()
+    public void jump()
     {
-        boolean isAnyKeyPressed = false;
-
-        if(Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            //enable debug
-            boolean debug = false;
-        }
-
-        //LEFT
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT) || this.controller.getAxis(XBox360Pad.AXIS_LEFT_X) < -0.2f) {
-            if(this.velocity.x > -SPEED_RUN_MAX_VELOCITY) {
-                if(this.velocity.x - SPEED_RUN_ACCELERATION < -SPEED_RUN_MAX_VELOCITY) {
-                    Vector2 force = new Vector2(-(SPEED_RUN_MAX_VELOCITY + this.velocity.x), 0);
-                    this.applyForce(force);
-                } else {
-                    Vector2 force = new Vector2(-SPEED_RUN_ACCELERATION, 0);
-                    this.applyForce(force);
-                }
-            }
-            isAnyKeyPressed = true;
-            this.direction = DIRECTION_LEFT;
-            if(!isJumping) {
-                this.changeAnimation(ACTION_RUN);
-            }
-        }
-
-        //RIGHT
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) || this.controller.getAxis(XBox360Pad.AXIS_LEFT_X) > 0.2f) {
-            if(this.velocity.x < SPEED_RUN_MAX_VELOCITY) {
-                if(this.velocity.x + SPEED_RUN_ACCELERATION > SPEED_RUN_MAX_VELOCITY) {
-                    Vector2 force = new Vector2(SPEED_RUN_MAX_VELOCITY - this.velocity.x, 0);
-                    this.applyForce(force);
-                } else {
-                    Vector2 force = new Vector2(SPEED_RUN_ACCELERATION, 0);
-                    this.applyForce(force);
-                }
-            }
-            isAnyKeyPressed = true;
-            this.direction = DIRECTION_RIGHT;
-            if(!isJumping) {
-                this.changeAnimation(ACTION_RUN);
-            }
-        }
-
-        //PARACHUTE
-        if((Gdx.input.isKeyPressed(Input.Keys.UP) || this.controller.getButton(XBox360Pad.BUTTON_X)) && Math.signum(this.velocity.y) == -1) {
-            this.friction = new Vector2(FRICTION_ON_PARACHUTE, 0);
-            this.velocity = new Vector2(this.velocity.x, -2.5f);
-        }
-
-        //JUMP
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && !this.hasDoubleJumped) {
+        if(!this.hasDoubleJumped) {
             if(this.isJumping) {
                 this.hasDoubleJumped = true;
             }
             this.isJumping = true;
             this.isOnGround = false;
+            this.isAnyKeyPressed = true;
             //ajust force to avoid velocity
             Vector2 force = new Vector2(0, SPEED_JUMP_ACCELERATION - this.velocity.y);
             this.applyForce(force);
             this.changeAnimation(ACTION_JUMP);
         }
+    }
 
-        //DASH LEFT
-        if((Gdx.input.isKeyJustPressed(Input.Keys.A) || this.controller.getAxis(XBox360Pad.AXIS_LEFT_TRIGGER) > 0.2f) && !(this.isDashingLeft || this.isDashingRight)) {
+    public void moveLeft()
+    {
+        if(this.velocity.x > -SPEED_RUN_MAX_VELOCITY) {
+            if(this.velocity.x - SPEED_RUN_ACCELERATION < -SPEED_RUN_MAX_VELOCITY) {
+                Vector2 force = new Vector2(-(SPEED_RUN_MAX_VELOCITY + this.velocity.x), 0);
+                this.applyForce(force);
+            } else {
+                Vector2 force = new Vector2(-SPEED_RUN_ACCELERATION, 0);
+                this.applyForce(force);
+            }
+        }
+        this.isAnyKeyPressed = true;
+        this.direction = DIRECTION_LEFT;
+        if(!isJumping) {
+            this.changeAnimation(ACTION_RUN);
+        }
+    }
+
+    public void moveRight()
+    {
+        if(this.velocity.x < SPEED_RUN_MAX_VELOCITY) {
+            if(this.velocity.x + SPEED_RUN_ACCELERATION > SPEED_RUN_MAX_VELOCITY) {
+                Vector2 force = new Vector2(SPEED_RUN_MAX_VELOCITY - this.velocity.x, 0);
+                this.applyForce(force);
+            } else {
+                Vector2 force = new Vector2(SPEED_RUN_ACCELERATION, 0);
+                this.applyForce(force);
+            }
+        }
+        this.isAnyKeyPressed = true;
+        this.direction = DIRECTION_RIGHT;
+        if(!isJumping) {
+            this.changeAnimation(ACTION_RUN);
+        }
+    }
+
+    public void deployParachute()
+    {
+        if(Math.signum(this.velocity.y) == -1) {
+            this.friction = new Vector2(FRICTION_ON_PARACHUTE, 0);
+            this.velocity = new Vector2(this.velocity.x, -2.5f);
+        }
+    }
+
+    public void dashLeft()
+    {
+        if(!(this.isDashingLeft || this.isDashingRight)) {
             this.isDashingLeft = true;
             this.elapsedDashDistance = 0f;
         }
+    }
 
-        //DASH RIGHT
-        if((Gdx.input.isKeyJustPressed(Input.Keys.E) || this.controller.getAxis(XBox360Pad.AXIS_RIGHT_TRIGGER) < -0.2f) && !(this.isDashingLeft || this.isDashingRight)) {
+    public void dashRight()
+    {
+        if(!(this.isDashingLeft || this.isDashingRight)) {
             this.isDashingRight = true;
             this.elapsedDashDistance = 0f;
-        }
-
-        if(!isAnyKeyPressed && !isJumping) {
-            if(Math.round(this.velocity.x * 100) == 0) {
-                this.velocity.x = 0;
-                changeAnimation(ACTION_STANCE);
-            }
         }
     }
 
@@ -252,78 +240,14 @@ public class Tengu extends Physics implements ControllerListener
         return this.currentAnimation.getHeight();
     }
 
-    public Tengu isOnGround(boolean isOnGround)
+    public Tengu setIsOnGround(boolean isOnGround)
     {
         this.isOnGround = isOnGround;
         return this;
     }
 
-    public Vector2 getFiction()
-    {
-        return this.friction;
-    }
-
     public Rectangle getRectangle(Rectangle rectangle)
     {
         return rectangle.setX(this.position.x).setY(this.position.y).setWidth(this.getWidth()).setHeight(this.getHeight());
-    }
-
-    @Override
-    public void connected(Controller controller) {
-
-    }
-
-    @Override
-    public void disconnected(Controller controller) {
-
-    }
-
-    @Override
-    public boolean buttonDown(Controller controller, int buttonCode) {
-
-        //JUMP
-        if (buttonCode == XBox360Pad.BUTTON_A && !this.hasDoubleJumped) {
-            if(this.isJumping) {
-                this.hasDoubleJumped = true;
-            }
-            this.isJumping = true;
-            this.isOnGround = false;
-            //ajust force to avoid velocity
-            Vector2 force = new Vector2(0, SPEED_JUMP_ACCELERATION - this.velocity.y);
-            this.applyForce(force);
-            this.changeAnimation(ACTION_JUMP);
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean buttonUp(Controller controller, int buttonCode) {
-        return false;
-    }
-
-    @Override
-    public boolean axisMoved(Controller controller, int axisCode, float value) {
-        return false;
-    }
-
-    @Override
-    public boolean povMoved(Controller controller, int povCode, PovDirection value) {
-        return false;
-    }
-
-    @Override
-    public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
-        return false;
-    }
-
-    @Override
-    public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
-        return false;
-    }
-
-    @Override
-    public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
-        return false;
     }
 }
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,21 +2,31 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.Controllers;
+import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Pool;
 import com.mygdx.character.Tengu;
 import com.mygdx.environment.Plateform;
 import com.mygdx.tools.Gravity;
+import com.mygdx.tools.XBox360Pad;
 
 import java.util.ArrayList;
 
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter implements ControllerListener, InputProcessor {
 
     private Gravity gravity;
 	private Tengu tengu;
+
+	Controller controller;
 
 	private Pool<Rectangle> rectanglePool = new Pool<Rectangle>() {
 		@Override
@@ -30,7 +40,15 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create ()
 	{
-        this.gravity = new Gravity();
+		// Listen to all controllers, not just one
+		Controllers.addListener(this);
+		Gdx.input.setInputProcessor(this);
+
+		if(Controllers.getControllers().size > 0) {
+			this.controller = Controllers.getControllers().first();
+		}
+
+		this.gravity = new Gravity();
 
 		this.tengu = new Tengu(new Vector2(0, 20), 0, 0);
         this.gravity.addGravityAffectedElement(this.tengu);
@@ -47,7 +65,11 @@ public class MyGdxGame extends ApplicationAdapter {
 		//apply gravity
 		this.gravity.applyGravity();
 
-		this.tengu.handleKeys();
+		//TODO Check if on keyboard or controller
+		this.handleKeyboard();
+		this.handleController();
+
+		//this.tengu.handleKeys();
 		this.tengu.update(deltaTime);
 
 		this.handleCollisions(oldTenguPosition);
@@ -121,8 +143,161 @@ public class MyGdxGame extends ApplicationAdapter {
 			this.rectanglePool.free(plateformRectangle);
 		}
 
-		this.tengu.isOnGround(isTenguOnGround);
+		this.tengu.setIsOnGround(isTenguOnGround);
 
 		this.rectanglePool.free(tenguRectangle);
+	}
+
+	public void handleKeyboard()
+	{
+		//MOVE LEFT
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			this.tengu.moveLeft();
+		}
+
+		//MOVE RIGHT
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			this.tengu.moveRight();
+		}
+
+		//PARACHUTE
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			this.tengu.deployParachute();
+		}
+	}
+
+	public void handleController()
+	{
+		if(this.controller != null) {
+			//MOVE LEFT
+			if(this.controller.getAxis(XBox360Pad.AXIS_LEFT_X) < -0.2f) {
+				this.tengu.moveLeft();
+			}
+
+			//MOVE RIGHT
+			if(this.controller.getAxis(XBox360Pad.AXIS_LEFT_X) > 0.2f ) {
+				this.tengu.moveRight();
+			}
+
+			//PARACHUTE
+			if(this.controller.getButton(XBox360Pad.BUTTON_X)) {
+				this.tengu.deployParachute();
+			}
+		}
+	}
+
+	@Override
+	public void connected(Controller controller) {
+
+	}
+
+	@Override
+	public void disconnected(Controller controller) {
+
+	}
+
+	@Override
+	public boolean buttonDown(Controller controller, int buttonCode) {
+		//JUMP
+		if(buttonCode == XBox360Pad.BUTTON_A) {
+			this.tengu.jump();
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean buttonUp(Controller controller, int buttonCode) {
+		return false;
+	}
+
+	@Override
+	public boolean axisMoved(Controller controller, int axisCode, float value) {
+		//DASH LEFT
+		if(axisCode == XBox360Pad.AXIS_LEFT_TRIGGER && value > 0.2f) {
+			this.tengu.dashLeft();
+		}
+
+		//DASH RIGHT
+		if(axisCode == XBox360Pad.AXIS_RIGHT_TRIGGER && value < -0.2f) {
+			this.tengu.dashRight();
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean povMoved(Controller controller, int povCode, PovDirection value) {
+		return false;
+	}
+
+	@Override
+	public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
+		return false;
+	}
+
+	@Override
+	public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
+		return false;
+	}
+
+	@Override
+	public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
+		return false;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		//JUMP
+		if(keycode == Input.Keys.SPACE) {
+			this.tengu.jump();
+		}
+
+		//DASH LEFT
+		if(keycode == Input.Keys.A) {
+			this.tengu.dashLeft();
+		}
+
+		//DASH RIGHT
+		if(keycode == Input.Keys.E) {
+			this.tengu.dashRight();
+		}
+
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
